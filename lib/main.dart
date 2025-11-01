795b713d29618a44bc31b2673c14367dbdf70733
vilihellsten <vilihellsten@hotmail.com>
2025-11-01 14:21:56 +0200

    Small change

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,12 @@ import 'package:flutter_matkapaivakirja/view/map_screen.dart';
 import 'package:flutter_matkapaivakirja/view/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   Get.put(SettingsController());
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
